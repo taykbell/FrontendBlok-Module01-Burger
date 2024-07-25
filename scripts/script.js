40249edf733acb930dd6fc7caf4d877f13df66e2
taykbell <ruslan3852@gmail.com>
2024-07-25 19:34:08 +0700

    Task number 23 completed

--- scripts/script.js
+++ scripts/script.js
@@ -42,3 +42,15 @@ function scrollFunction(){
     }
 }
 
+
+       // Menu
+
+       function openModal() {
+        document.getElementById("modal").style.top = "0px";
+    }
+
+    function closeModal() {
+        document.getElementById("modal").style.top = "-400px";
+    }
+
+
